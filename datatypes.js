e4413a5586d6cf145c3622087159e2656df43ddf
DA M1 37 <dam137@DAs-MacBook-Air.local>
2025-07-11 09:24:23 +0200

    first  datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+let petName = 'Tman' // Global variable
+myFunction()
+function myFunction() {
+fruit = 'orange'; // Considered global
+console.log(typeof petName + '- ' + 'My pet name is ' +petName)}
+
+function calcAddition(number1, number2) {
+return number1 + number2;}
+
+console.log(calcAddition(6,9));
+
+var a = 10
+function f() {
+console.log(a)}
+f();
+console.log(a);
