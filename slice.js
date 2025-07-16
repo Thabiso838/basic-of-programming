464e6a0ace5130b89b6a5a26243b295372cd4dab
DA M1 37 <dam137@DAs-MacBook-Air.local>
2025-07-16 09:23:01 +0200

    slice

---
+++ slice.js
@@ -0,0 +1,5 @@
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1);
+document.getElementById("demo").innerHTML = fruits + "<br><br>" + citrus;
+
+console.log(fruits);
